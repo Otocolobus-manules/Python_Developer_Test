f4fba6d3a8e5efe4e854300a983e740e5b301063
Чушка подзаборная <125672053+Otocolobus-manules@users.noreply.github.com>
2024-06-30 12:17:58 +0300

    defining routes for the user

--- src/api/api_v1/users.py
+++ src/api/api_v1/users.py
@@ -1,3 +1,29 @@
 from fastapi import APIRouter
+from core.schemas.user import UserRead, UserCreate, UserUpdate, UserDelete
+from core.services.user_service import user_service
 
 user_router = APIRouter(tags=['Users'])
+
+
+@user_router.post('/create_user', response_model=UserRead)
+async def create_user_api(user: UserCreate):
+    result = await user_service.create_user(user=user)
+    return result
+
+
+@user_router.put('/update_user', response_model=UserRead)
+async def update_user_api(user: UserUpdate):
+    result = await user_service.update_user(user=user)
+    return result
+
+
+@user_router.get('/get_user', response_model=UserRead)
+async def get_user_api(user_id: int):
+    result = await user_service.get_user(user_id=user_id)
+    return result
+
+
+@user_router.delete('/delete_user', response_model=UserRead)
+async def delete_user_api(user: UserDelete):
+    result = await user_service.delete_user(user=user)
+    return result
