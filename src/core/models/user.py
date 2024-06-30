ff87a0b8b58a5c260bd681cd2eafb715b0d1ce0b
Чушка подзаборная <125672053+Otocolobus-manules@users.noreply.github.com>
2024-06-30 10:52:33 +0300

    defining the user model

---
+++ src/core/models/user.py
@@ -0,0 +1,8 @@
+from .base import Base
+from sqlalchemy.orm import Mapped, mapped_column
+
+
+class User(Base):
+    __tablename__ = 'user'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(nullable=False)
