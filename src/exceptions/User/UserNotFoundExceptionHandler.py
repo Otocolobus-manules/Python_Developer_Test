0d041ce834e198dbc6beb7c37e8457541b8e943b
Чушка подзаборная <125672053+Otocolobus-manules@users.noreply.github.com>
2024-07-26 13:37:27 +0300

    Добавление или дополнение документации

--- src/exceptions/User/UserNotFoundExceptionHandler.py
+++ src/exceptions/User/UserNotFoundExceptionHandler.py
@@ -4,6 +4,13 @@ from exceptions.User.UserNotFoundException import UserNotFoundException
 
 
 def user_not_found_exception_handler(request: Request, exc: UserNotFoundException):
+    """
+        Обработчик исключений для UserNotFoundException.
+
+        :param request: Объект запроса FastAPI.
+        :param exc: Исключение UserNotFoundException.
+        :return: JSON-ответ с кодом состояния 404 и сообщением об ошибке.
+        """
     return JSONResponse(
         status_code=404,
         content={"message": exc.message},
