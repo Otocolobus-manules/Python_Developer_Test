cdd05ab23b9d3ad7057e10f1f537dd56676da58b
Чушка подзаборная <125672053+Otocolobus-manules@users.noreply.github.com>
2024-07-26 13:00:48 +0300

    Добавлено fastapi приложение

--- src/app/fastapi_app.py
+++ src/app/fastapi_app.py
@@ -2,10 +2,18 @@ import uvicorn
 from fastapi import FastAPI
 
 from utils.IoC import IoC
+from routing.UserCrudRouter import UserCrudRouter
+from exceptions.User.UserNotFoundExceptionHandler import user_not_found_exception_handler
+from exceptions.User.UserNotFoundException import UserNotFoundException
 
 
 async def fastapi_app(container: IoC):
     app = FastAPI()
+
+    user_crud_router = UserCrudRouter(container=container).get_router()
+
+    app.include_router(user_crud_router)
+    app.add_exception_handler(UserNotFoundException, user_not_found_exception_handler)
 
     config = uvicorn.Config(
         app,
